56b022a947ca69705fec5e3f1072036b028f9879
omerdal2002@gmail.com <omerdal2002@gmail.com>
2021-01-18 18:04:21 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + musteri.Id + "  " + musteri.Adi + "  " + musteri.Soyadi + "  " + musteri.Yasi);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : " + musteri.Id + "  " + musteri.Adi + "  " + musteri.Soyadi + "  " + musteri.Yasi);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Musteri[] musteriler = new Musteri[] { musteri };
+
+            foreach (var listele in musteriler)
+            {
+                Console.WriteLine(musteri.Id + "\n" + musteri.Adi + "\n" + musteri.Soyadi + "\n" + musteri.Yasi);
+
+            }
+
+        }
+    }
+}
